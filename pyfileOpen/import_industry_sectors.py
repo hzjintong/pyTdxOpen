4842d9d0f6442cdbc167ae5e5f73cea99916d6ce
jin tong <hzjintong@gmail.com>
2026-08-07 10:23:42 +0800

    chang some thing.

--- pyfileOpen/import_industry_sectors.py
+++ pyfileOpen/import_industry_sectors.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 # 🎯 您的核心配置路徑
 DB_PATH = "E:/tdx_financial.db"
-TXT_PATH = "D:/二级行业板块.txt"
+TXT_PATH = "D:/行业板块.txt"
 
 # 🎯 核心防禦開關：如果表結構發生衝突報錯，將此處改為 True，運行一次後會自動洗表重構
 FORCE_REBUILD = False  # 遇到表結構衝突報，需要变更时改為 True ，也可以直接删除表，或手工增江两个字段，以便保留已经手工维护进去的退市股票
